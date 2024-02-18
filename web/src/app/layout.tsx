0dae7f467355cf07116044f453c39816ae39e7b8
mako <827652549@qq.com>
2024-02-18 21:39:22 +0800

    feat: add vercel analytics

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,8 @@ export default function RootLayout({
       <body className={inter.className}>
         Root Layout.tsx balabala~
         {children}
-        </body>
+        <Analytics/>
+      </body>
     </html>
   );
 }
